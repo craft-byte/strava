892f05eff5a1e6fcd2a0c18c8e23c365f31fb442
Bazhan Nagnybida <bazhantt@gmail.com>
2022-10-15 11:11:37 -0400

    animations disabled on phones and better ui for phones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,29 @@ import { HashLocationStrategy } from '@angular/common';
 import { Interceptor } from './other/interceptor';
 import { NgxStripeModule } from 'ngx-stripe';
 
-const disableAnimations =
-  !('animate' in document.documentElement)
-  || (navigator && /iPhone OS (8|9|10|11|12|13)_/.test(navigator.userAgent));
+const disableAnimations = () => {
+
+    if (
+        !('animate' in document.documentElement)
+        || (navigator && /iPhone OS (8|9|10|11|12|13)_/.test(navigator.userAgent))
+    ) {
+        return true;
+    }
+
+    if(window.innerWidth < 1000) {
+        return true;
+    }
+
+    return false;
+}
 
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule.withConfig({ disableAnimations }),
-        SocketIoModule.forRoot({ url: environment.socketLink, options: {  } }),
+        BrowserAnimationsModule.withConfig({ disableAnimations: disableAnimations() }),
+        SocketIoModule.forRoot({ url: environment.socketLink, options: {} }),
         IonicModule.forRoot(),
         NgxStripeModule.forRoot("pk_test_51KNlK6LbfOFI72xW4xnsuE6JQRte49N0HFiLw9mfQn8JF1JuImLOr2QJZewBZwXiPRNgsS6ebeiOisn3Gebp0zLT00i143bkrp"),
         AppRoutingModule,
