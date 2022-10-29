4cdccf9b12b7a33344c164202a5e1db642f7d1fd
Bazhan Nagnybida <bazhantt@gmail.com>
2022-10-29 12:26:06 -0400

    style: modal animations added and responsive design

--- src/app/restaurant/settings/change-name/change-name.component.ts
+++ src/app/restaurant/settings/change-name/change-name.component.ts
@@ -1,3 +1,4 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -11,6 +12,29 @@ import { RestaurantService } from '../../services/restaurant.service';
     styleUrls: ['./change-name.component.scss'],
     standalone: true,
     imports: [CommonModule, FormsModule, IonicModule],
+    animations: [
+        trigger("showUp", [
+            transition(":enter", [
+                group([
+                    style({ backgroundColor: "rgba(0,0,0,0)" }),
+                    animate("150ms ease-in", style({ backgroundColor: "rgba(0,0,0,0.25)" })),
+                    query(".box", [
+                        style({ opacity: "0", scale: "0.7" }),
+                        animate("150ms ease-in", style({ opacity: "1", scale: "1" })),
+                    ])
+                ])
+            ]),
+            transition(":leave", [
+                group([
+                    animate("150ms ease-in", style({ backgroundColor: "rgba(0,0,0,0)" })),
+                    query(".box", [
+                        style({ opacity: "1", scale: "1" }),
+                        animate("150ms ease-in", style({ opacity: "0", scale: "0.7" })),
+                    ])
+                ])
+            ]),
+        ])
+    ]
 })
 export class ChangeNameComponent implements OnInit {
     newName: string;
