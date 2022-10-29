7be13ca3e38af6912351c430375f056a5a2365b6
Bazhan Nagnybida <bazhantt@gmail.com>
2022-10-29 14:40:12 -0400

    fix: allowTakeAway is false by default

--- server/src/routers/user/addRestaurant.ts
+++ server/src/routers/user/addRestaurant.ts
@@ -207,7 +207,7 @@ router.post("/create", logged({ email: 1, status: 1 }), confirmed(true), async (
         },
         customers: {
             maxDishes: 10,
-            allowTakeAway: true,
+            allowTakeAway: false,
             // trust: 1,
             maxCustomers: 10,
             // maxPrice: "unlimited",
