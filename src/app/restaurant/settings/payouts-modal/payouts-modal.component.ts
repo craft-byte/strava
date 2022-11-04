7010ff47fc9a1e8185a470b8b2c16f70c019202e
Bazhan Nagnybida <bazhantt@gmail.com>
2022-11-03 23:18:56 -0400

    fix: animations added

--- src/app/restaurant/settings/payouts-modal/payouts-modal.component.ts
+++ src/app/restaurant/settings/payouts-modal/payouts-modal.component.ts
@@ -1,3 +1,4 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
@@ -8,7 +9,30 @@ import { RouterService } from 'src/app/other/router.service';
     templateUrl: './payouts-modal.component.html',
     styleUrls: ['./payouts-modal.component.scss'],
     standalone: true,
-    imports: [CommonModule, IonicModule]
+    imports: [CommonModule, IonicModule],
+    animations: [
+        trigger("showUp", [
+            transition(":enter", [
+                group([
+                    style({ backgroundColor: "rgba(0,0,0,0)" }),
+                    animate("150ms ease-in", style({ backgroundColor: "rgba(0,0,0,0.25)" })),
+                    query(".box", [
+                        style({ opacity: "0", scale: "0.7" }),
+                        animate("150ms ease-in", style({ opacity: "1", scale: "1" })),
+                    ])
+                ])
+            ]),
+            transition(":leave", [
+                group([
+                    animate("150ms ease-in", style({ backgroundColor: "rgba(0,0,0,0)" })),
+                    query(".box", [
+                        style({ opacity: "1", scale: "1" }),
+                        animate("150ms ease-in", style({ opacity: "0", scale: "0.7" })),
+                    ])
+                ])
+            ]),
+        ])
+    ]
 })
 export class PayoutsModalComponent implements OnInit {
 
