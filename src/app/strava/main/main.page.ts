1dd1b78ff2e02ce810218ad543acde9b3779581e
Bazhan Nagnybida <bazhantt@gmail.com>
2022-11-19 22:54:28 -0500

    fix: nothing new

--- src/app/strava/main/main.page.ts
+++ src/app/strava/main/main.page.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { LoadService } from 'src/app/other/load.service';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.page.html',
-  styleUrls: ['./main.page.scss'],
+    selector: 'app-main',
+    templateUrl: './main.page.html',
+    styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
 
-  constructor(
-    private loader: LoadService,
-  ) { }
+    constructor(
+        private loader: LoadService,
+    ) { }
 
-  ngOnInit() {
-    this.loader.end();
-  }
+    ngOnInit() {
+        this.loader.end();
+    }
 
 }
