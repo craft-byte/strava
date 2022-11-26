9a84a2803aa47eba681fc54f63fe05dccfeb711c
Bazhan Nagnybida <bazhantt@gmail.com>
2022-11-25 20:34:45 -0500

    fix: confirm order function moved to separate file

--- server/src/routers/stripe.ts
+++ server/src/routers/stripe.ts
@@ -9,6 +9,7 @@ import { getDelay } from "../utils/other";
 import { updateUser } from "../utils/users";
 import { Order } from "../models/general";
 import { StripeOrderMetadata } from "../models/other";
+import { confirmOrder } from "../utils/confirmOrder";
 
 
 const router = Router();
@@ -105,11 +106,11 @@ router.post("/account-webhook", e.raw({ type: 'application/json' }), async (req,
 
         if (data.metadata.orderId && data.metadata.restaurantId) {
 
-            const { orderId, restaurantId, by } = data.metadata as StripeOrderMetadata;
+            const { orderId, restaurantId, } = data.metadata as StripeOrderMetadata;
 
-            console.log("on order payed called");
-            
-            onOrderPayed(restaurantId, orderId, by);
+            console.log("ORDER CONFIRMED");
+
+            confirmOrder(restaurantId, orderId, "card");
             
         }
 
@@ -136,74 +137,6 @@ router.get("/", (req, res) => {
     res.send({ hello: true });
 });
 
-
-
-
-
-
-
-async function onOrderPayed(restaurantId: string, orderId: string, by: "customer" | "staff") {
-
-    console.log(restaurantId, orderId, by);
-
-    // set order type to progress (cooking)
-    const update = await Orders(restaurantId).one({ _id: id(orderId) })
-        .update(
-            { $set: { status: "progress", method: "card", ordered: Date.now() } },
-            { returnDocument: "after", projection: { dishes: 1, onBefalf: 1, socketId: 1, ordered: 1, customer: 1, } }
-        );
-
-    const order = update.order;
-
-    if(by && order.socketId) {
-        sendMessage([order.socketId], "customer", { orderId: order._id, payed: true, type: "payment.succeeded" });
-    }
-
-
-    //     \/  send dishes to kitchen  \/
-    const ids = new Set<string>();
-
-    for (let i of order.dishes!) {
-        ids.add(i.dishId.toString());
-    }
-
-    const dishes = await Restaurant(restaurantId).dishes.many({ _id: { $in: Array.from(ids).map(a => id(a)) } }).get({ projection: { general: 1, } });
-
-    const forKitchen = [];
-    const time = getDelay(order.ordered!);
-    for (let i in order.dishes!) {
-        for (let { general, _id } of dishes) {
-            if (_id.equals(order.dishes[i].dishId)) {
-                forKitchen.push({
-                    orderId: order._id,
-                    ...order.dishes![i],
-                    time,
-                    general: general
-                });
-            }
-        }
-    }
-
-
-    sendMessage([`${restaurantId}/kitchen`], "kitchen", {
-        type: "kitchen/order/new",
-        event: "kitchen",
-        data: forKitchen,
-    });
-    //     /\  send dishes to kitchen  /\
-
-
-
-    // update dishes bought count
-    Restaurant(restaurantId).dishes.many({ _id: { $in: Array.from(ids).map(a => id(a)) } }).update({ $inc: { "info.bought": 1 } });
-
-
-    
-    // add order to user history
-    if(by == "customer" && order.customer) {
-        updateUser({ _id: id(order.customer) }, { $push: { orders: { restaurantId: id(restaurantId), orderId: id(orderId) } } });
-    }
-}
 
 
 async function onOrderPaymentFailed(restaurantId: string, orderId: string, customerId: string) {
---
+++ server/src/utils/confirmOrder.ts
@@ -0,0 +1,155 @@
+import { ObjectId } from "mongodb";
+import { Order } from "../models/general";
+import { id } from "./functions";
+import { sendMessage } from "./io";
+import { getDelay } from "./other";
+import { Orders, Restaurant } from "./restaurant";
+import { updateUser } from "./users";
+
+
+
+/**
+ * 
+ * orders are confirmed at:
+ *      - src/routers/stripe.ts
+ *      - src/routers/staff/manualOrder.ts
+ *      - src/routers/customers/order.ts
+ * 
+ */
+
+
+
+/**
+ * 
+ * - updates the order status
+ * - increases bought property to the bought dishes
+ * - sends data to kitchen
+ * 
+ * @param { string | ObjectId } restaurantId
+ * @param { string | ObjectId } orderId
+ * 
+ */
+export async function confirmOrder(restaurantId: string | ObjectId, orderId: string | ObjectId, method: "card" | "cash") {
+    
+
+    // set order type to progress (cooking)
+    const update = await Orders(restaurantId).one({ _id: id(orderId) })
+        .update(
+            { $set: {
+                status: "progress",
+                method,
+                ordered: Date.now()
+            } },
+            {
+                returnDocument: "after",
+                projection: {
+                    dishes: 1,
+                    onBefalf: 1,
+                    socketId: 1,
+                    customer: 1,
+                }
+            }
+        );
+
+
+    await sendData(restaurantId, update.order);
+
+    await updateDishes(restaurantId, update.order.dishes);
+
+    
+    if(update.order.customer) {
+        await updateUserHistory(restaurantId, update.order);
+    }
+}
+
+
+/**
+ * 
+ * converts data and sends to kitchen with socket.io
+ * 
+ * @param { string | ObjectId } restaurantId 
+ * @param { Order } order 
+ */
+async function sendData(restaurantId: string | ObjectId, order: Order) {
+
+    if(order.socketId) {
+        // sends user to the tracking
+        sendMessage([order.socketId], "customer", { orderId: order._id, payed: true, type: "payment.succeeded" });
+    }
+
+
+    const ids = new Set<string>();
+
+    for (let i of order.dishes!) {
+        ids.add(i.dishId.toString());
+    }
+
+    const dishes = await Restaurant(restaurantId).dishes.many({ _id: { $in: Array.from(ids).map(a => id(a)) } }).get({ projection: { general: 1, } });
+
+    const forKitchen = [];
+    const time = getDelay(order.ordered!);
+    for (let i in order.dishes!) {
+        for (let { general, _id } of dishes) {
+            if (_id.equals(order.dishes[i].dishId)) {
+                forKitchen.push({
+                    orderId: order._id,
+                    ...order.dishes![i],
+                    time,
+                    general: general
+                });
+            }
+        }
+    }
+
+
+    sendMessage([`${restaurantId}/kitchen`], "kitchen", {
+        type: "kitchen/order/new",
+        event: "kitchen",
+        data: forKitchen,
+    });
+}
+
+
+/**
+ * 
+ * converts data and sends to kitchen with socket.io
+ * 
+ * @param { string | ObjectId } restaurantId 
+ * @param { Order["dishes"] } dishes 
+ */
+async function updateDishes(restaurantId: string | ObjectId, dishes: Order["dishes"]) {
+    
+    const data: { [dishId: string]: number } = {};
+
+    for(let i of dishes) {
+        const dishId = i.dishId.toString();
+
+        if(data[dishId]) {
+            data[dishId]++;
+        } else {
+            data[dishId] = 1;
+        }
+    }
+    
+    for(let dishId of Object.keys(data)) {
+        Restaurant(restaurantId).dishes.one(dishId).update({ $inc: { "info.bought": data[dishId] } });
+    }
+}
+
+
+/**
+ * 
+ * adds order to the user's history
+ * 
+ * @param { string | ObjectId } restaurantId 
+ * @param { Order } order 
+ */
+async function updateUserHistory(restaurantId: string | ObjectId, order: Order) {
+    
+    if(order.customer) {
+
+        const update = await updateUser({ _id: id(order.customer) }, { $push: { orders: { restaurantId: id(restaurantId), orderId: id(order._id) } } });
+
+    }
+
+}
